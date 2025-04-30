9636d61aa1f9a2678ef5e27d118ed598a53fb0c6
mizutanigrandee <t.mizutani@thegrandee.co.jp>
2025-04-30 18:53:51 +0900

    Update vacancy_app.py

--- vacancy_app.py
+++ vacancy_app.py
@@ -13,14 +13,8 @@ import os, json, pytz, jpholiday
 
 st.set_page_config(page_title="【超いいツール】ミナミエリア 空室＆平均価格カレンダー", layout="wide")
 banner = Image.open("バナー画像.png")
-st.markdown(
-    """
-    <div style="text-align: center;">
-        <img src="バナー画像.png" width="600">
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+st.image("バナー画像.png", width=600)
+
 
 
 
