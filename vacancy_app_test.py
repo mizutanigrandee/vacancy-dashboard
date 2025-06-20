337126278018277411e05db30fdc7db0d8c64797
mizutanigrandee <t.mizutani@thegrandee.co.jp>
2025-06-20 16:13:04 +0900

    Update vacancy_app_test.py

--- vacancy_app_test.py
+++ vacancy_app_test.py
@@ -258,7 +258,7 @@ elif selected_date and st.session_state["show_graph"]:
     left, right = st.columns([3, 7])
     with left:
         # タイトル下に3ボタンを横並び・左寄せで配置
-        button_cols = st.columns([10, 5, 5, 5])  # [閉じる][前日][翌日][空き]
+        button_cols = st.columns([5, 5, 5])  # [閉じる][前日][翌日]
         with button_cols[0]:
             if st.button("❌ 閉じる"):
                 st.query_params.clear()
