c7e0201bc4de6cd1577b1cee37c5d7826a06d94c
mizutanigrandee <t.mizutani@thegrandee.co.jp>
2025-06-21 10:22:41 +0900

    Update vacancy_app_test.py

--- vacancy_app_test.py
+++ vacancy_app_test.py
@@ -245,7 +245,7 @@ else:
             .mark_line(point=True)
             .encode(
                 x=alt.X("取得日:T", axis=alt.Axis(title=None, format="%m/%d")),
-                y=alt.Y("在庫数:Q", axis=alt.Axis(title=None, scale=alt.Scale(domain=[0, 350])))
+                y=alt.Y("在庫数:Q", axis=alt.Axis(title=None), scale=alt.Scale(domain=[0, 350]))
             )
             .properties(height=320, width=600)
         )
@@ -256,11 +256,12 @@ else:
             .mark_line(point=True, color="#e15759")
             .encode(
                 x=alt.X("取得日:T", axis=alt.Axis(title=None, format="%m/%d")),
-                y=alt.Y("平均単価:Q", axis=alt.Axis(title=None, scale=alt.Scale(domain=[0, 35000])))
+                y=alt.Y("平均単価:Q", axis=alt.Axis(title=None), scale=alt.Scale(domain=[0, 35000]))
             )
             .properties(height=320, width=600)
         )
         st.altair_chart(chart_price, use_container_width=True)
+
 
 
         st.write("##### 平均単価 (円)")
